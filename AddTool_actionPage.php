0ef183d0fac7918daaece9e43ba92a228366f13d
NoorAlsharif <nalsharif0045@stu.kau.edu.sa>
2022-04-25 08:21:20 +0300

    nnew

---
+++ AddTool_actionPage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>forDB</title>
+</head>
+
+    <body>
+	<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => HUA_store
+		$conn = mysqli_connect("localhost", "root", "", "HUA_store");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$Tool_name = $_REQUEST['Tname'];
+		$Tool_status = $_REQUEST['Tstatus'];
+		$Tool_price = $_REQUEST['Tprice'];
+        $Tool_city = $_REQUEST['Tcity'];
+
+		$Tool_section = $_REQUEST['Tsection'];
+        $Tool_type = $_REQUEST['Ttype'];
+
+
+        $Tool_moreinfo = $_REQUEST['Tminfo'];
+        $Tool_pic = $_REQUEST['Tpics'];
+        $Tool_vid = $_REQUEST['Tvid'];
+        
+		// Performing insert query execution
+		// here our table name is AddTools
+		$sql = "INSERT INTO AddTools VALUES (
+            '$Tool_name',
+            '$Tool_status',
+            '$Tool_price',
+            '$Tool_city',
+
+            '$Tool_section',
+            '$Tool_type',
+
+            '$Tool_moreinfo',
+            '$Tool_pic',
+            '$Tool_vid')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+			echo nl2br("\n$first_name\n $last_name\n "
+				. "$gender\n $address\n $email");
+		} else{
+			echo "ERROR: $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+	?>
+</body>
+
+</html>
